30b1bde996fadb3ff9b4fc3cf74b7d4a0c09882a
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-06-08 21:13:03 +0000

    authenticate enrolled users for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
+  
   def show
   end
   
   private
+  
+  def require_enrolled_for_current_course
+    if !(current_user && current_user.enrolled_in?(current_lesson.section.course))
+      redirect_to course_path(@current_lesson.section.course), alert: 'You need to enroll before watching lessons!'
+    end
+  end
+  
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
